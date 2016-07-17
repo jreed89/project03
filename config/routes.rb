d916a2f6b936dfc78bee4be4be424367b66b2517
Joshua Daniel <jrdaniel@ncsu.edu>
2016-07-17 16:07:58 -0400

    changed root to boxes#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
- root to: "items#index"
+ root to: "boxes#index"
 
  get "items", to: "items#index"
  get "items/new", to: "items#new"
