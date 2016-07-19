4d317f8a5ac882752c36d704bc2eab3a7a511e93
Joshua Daniel <jrdaniel@ncsu.edu>
2016-07-19 09:11:36 -0400

    made a breakthrough with the edit button

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
 
  get "items", to: "items#index"
  get "items/new", to: "items#new"
+ get "items/edit/:id", to: "items#edit", as: :item_edit
  get "items/:id", to: "items#show", as: :item
  post "items", to: "items#create"
- get "items/:id/edit", to: "items#edit", as: :item_edit
+
  patch "items/:id", to: "items#update"
  delete "items/:id", to: "items#destroy"
 
@@ -20,6 +21,10 @@ Rails.application.routes.draw do
  get "boxes/:id/edit", to: "boxes#edit", as: :box_edit
  patch "boxes/:id", to: "boxes#update"
  delete "boxes/:id", to: "boxes#destroy"
+# root 'boxes#index'
+#   resources :boxes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+#     resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
+# end
 
 end
